49b0424bc733f31fbd33e96fb42515f90e7f988c
khigginsc <kevinhiggins301@gmail.com>
2020-10-11 16:37:54 -0500

    Rest API & Mongodb

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,7 +1,28 @@
 const { Router } = require('express');
-const { modelNames } = require('mongoose');
+const { closeSync } = require('fs-extra');
+const Book = require('../models/Book');
 const router = Router();
 
-router.get('/', (req, res) => res.json({text: 'Hola Kevin'}));
+require('../models/Book');
+
+router.get('/', async(req, res) => {
+    const books = await Book.find();
+    res.json(books);
+
+});
+
+router.post('/', async (req, res) => {
+    const {title, author, isbn} = req.body;
+   const newBook = new Book({title, author, isbn});
+   await newBook.save();
+    console.log(newBook);
+    res.send({message: 'Book Save'});
+});
+
+router.delete('/:id', async (req, res) => {
+    const DeleteBook = Book.findByIdAndDelete(req.params.id);
+    console.log(DeleteBook);
+    res.send({message: 'Book Delete'});
+})
 
 module.exports = router;
